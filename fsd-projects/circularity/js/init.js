692150e01f49746b54391fcb89eb83c62eb54c8b
ShayneGeorge7339972 <skg7339972@lpssonline.com>
2026-03-31 16:55:01 +0000

    todo 8 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -93,7 +93,26 @@ var init = function (window) {
            
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+           physikz.updatePosition(circles[ 0 ])
+
+physikz.updatePosition(circles[ 1 ])
+
+physikz.updatePosition(circles[ 2 ])
+
+physikz.updatePosition(circles[ 3 ])
+
+physikz.updatePosition(circles[ 4 ])
+
+for (var i = 0; i < circles.length; i++) {
+
+               physikz.updatePosition(circles[i]);
+
+               game.checkCirclePosition(circles[i]);
+
+
+}
+
+
             
         }
     
